e112fbce92d1ed9bff142240fd733767c7f6a6dd
Ngheizit <ngheizitwu@163.com>
2019-09-28 10:41:56 +0800

    20190928 add baidu info

---
+++ default-js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?74ff606bd0b9304890d1c65f51f1225c";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+})();       
