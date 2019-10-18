5beeef59fdc0e8bcc695114dc130b35c21dbc967
Ngheizit <ngheizitwu@163.com>
2019-10-18 21:09:02 +0800

    20191018 map jiagou

--- ArcGIS API for JS/Calcite Maps and Bootstrap/index.js
+++ ArcGIS API for JS/Calcite Maps and Bootstrap/index.js
@@ -32,19 +32,23 @@ function Main(){
          * App setting
          */
         window.app = {
-            center: [-40, 40],
-            scale: 50000000,
-            basemap: "streets",
+            center: [103, 23], // 经纬度
+            scale: 60000000, // 缩放比例
+            basemap: "streets", // 底图源
             viewPadding: {
                 top: 50,
                 bottom: 0
             },
-            uiComponents: ["zoom", "compass", "attribution"],
+            // 默认地图控件
+            uiComponents: [
+                // "zoom", // 缩放
+                // "compass" // 罗盘
+                // "attribution" // 地图脚注
+            ],
             mapView: null,
-            sceneView: null,
             containerMap: "axMapView",
             containerScene: "axSceneView",
-            activeView: null,
+            activeView: null, // 当前视窗下（被激活）的视图（MapView or SceneView）
             searchWidget: null
         };
         /**
@@ -78,9 +82,50 @@ function Main(){
             }
         });
         CalciteMapsArcGIS.setPopupPanelSync(app.sceneView);
+
+        
+
+
+        // 设置当前激活状态的视图（MapView or Scene View）
         var setActiveView = function(view){
             app.activeView = view
         };
-        setActiveView(app.mapView);
+        // 切换视图时，同步MapView视图和SceneView视图位置
+        var syncViews = function(fromView, toView){
+            const viewPt = fromView.viewpoint.clone();
+            if(fromView.type === "3d"){ // MapView => SceneView
+                toView.container = app.containerMap;
+            }else{ // MapView => SceneView
+                toView.container = app.containerScene;
+            }
+            toView.padding = app.viewPadding;
+            toView.viewpoint = viewPt;
+        };
+        setActiveView(app.mapView); // 默认激活MapView视图
+
+        // 地图切换与同步
+        const tabs = Array.from(document.querySelectorAll(".calcite-navbar li a[data-toggle='tab']"));
+        tabs.forEach(function(tab){
+            tab.addEventListener("click", function(event){
+                if(event.target.text.indexOf("Map") > -1){ // 切换为SceneView视图
+                    syncViews(app.sceneView, app.mapView);
+                    setActiveView(app.mapView);
+                }else{ // 切换为MapView视图
+                    syncViews(app.mapView, app.sceneView);
+                    setActiveView(app.sceneView);
+                }
+            });
+        });
+
+        // 底图选择功能
+        // Create basemap widget
+        app.basemapWidget = new BasemapGallery({
+            view: app.activeView,
+            container: "axBasemapPanel"
+        });
+
+        // Settings 设置
+        
+        
     });
 }
