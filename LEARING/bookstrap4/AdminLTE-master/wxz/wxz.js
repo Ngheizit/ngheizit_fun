c5403f00426a999cba18a2bb7ec0689fafc6961f
Ngheizit <ngheizitwu@163.com>
2019-10-31 15:30:00 +0800

    20191031 wxz.js

--- LEARING/bookstrap4/AdminLTE-master/wxz/wxz.js
+++ LEARING/bookstrap4/AdminLTE-master/wxz/wxz.js
@@ -1,24 +1,65 @@
 window.onload = function(){
 
-    // css
-    let content = document.querySelectorAll('.content-wrapper')[0];
-    console.log(content.offsetHeight);
-    document.getElementById('axMapView').style.height = content.offsetHeight + 'px';
-    console.log(document.getElementById('axMapView'));
+    // css 动态设置地图控件高度
+    let content = document.querySelectorAll('.main-sidebar')[0];
+    document.getElementById('axMapView').style.height = content.clientHeight - document.querySelectorAll('.main-header')[0].clientHeight + 'px';
+
+    window.bool_sketch = false;
 
     this.Main();
 }
 function Main(){
-    require(["esri/Map", "esri/views/MapView"],function(Map, MapView){
-    
-        var map = new Map({
-            basemap: "streets"
+    require([
+        "esri/Map", 
+        "esri/views/MapView",
+        "esri/widgets/Sketch",
+        "esri/layers/GraphicsLayer"
+    ],
+    function(
+        Map, 
+        MapView,
+        Sketch,
+        GraphicsLayer
+    ){
+        let graphicsLayer = new GraphicsLayer();
+        let map = new Map({
+            basemap: "streets",
+            layers: [graphicsLayer]
         });
-        var view = new MapView({
+        let view = new MapView({
             container:"axMapView",
             map:map,
             zoom:  4,
             center:[15,65]
+        });
+
+        // 清除地图底部说明标注函数
+        let ClearWidgetsOfBottom = function(){
+            let widget_bottom = document.getElementsByClassName("esri-component esri-attribution esri-widget")[0];
+            widget_bottom.remove();
+        };
+
+        // 控制sketch绘制图形控件开关函数
+        let OpenOrCloseSketch = function(){
+            let sketch = new Sketch({
+                view: view,
+                layer: graphicsLayer
+            });
+            let btn = document.getElementById('btn-drawGraphics');
+            btn.addEventListener('click', function(){
+                window.bool_sketch = !window.bool_sketch;
+                if(window.bool_sketch){
+                    view.ui.add(sketch, "top-right");
+                }else{
+                    view.ui.remove(sketch);
+                }
+            });
+        };
+
+        // 地图视图加载完成后触发此事件
+        view.when(function(){
+            ClearWidgetsOfBottom();
+            OpenOrCloseSketch();
         });
 
     });
